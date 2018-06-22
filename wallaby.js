2d0a24f94495bbb50e2d354e8ee9824225bf98f8
HenryNguyen5 <ngu.henry@hotmail.com>
2018-06-22 14:21:13 -0400

    Add wallaby support (for editor integration)

---
+++ wallaby.js
@@ -0,0 +1,26 @@
+module.exports = function(wallaby) {
+	process.env.NODE_ENV = "development";
+
+	return {
+		name: "mymonero-core-js",
+		files: [
+			"cryptonote_utils/**/*.js",
+			"hostAPI/**/*.js",
+			"monero_utils/**/*.js",
+			"index.js",
+		],
+
+		filesWithNoCoverageCalculated: [
+			"cryptonote_utils/nacl-fast-cn.js",
+			"cryptonote_utils/biginteger.js",
+			"cryptonote_utils/sha3.js",
+			"cryptonote_utils/cryptonote_crypto_EMSCRIPTEN.js",
+		],
+
+		tests: ["./tests/**/*spec.js"],
+
+		testFramework: "jest",
+
+		env: { type: "node", runner: "node" },
+	};
+};
