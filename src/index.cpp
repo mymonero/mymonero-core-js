59a383c7c1c950097512b01afe97c72bffea047f
Paul Shapiro <paulshapiro@users.noreply.github.com>
2018-08-21 14:14:39 -0400

    bridged new serial_bridge_index methods and added all manual call test cases in MyMoneroCoreCpp_int.node.js - pending replacing JS API fns with these calls. this commit shows the first working POC of transpiling the major monero src to JS, and has shown that startup time must be optimized. that's currently a WIP.. contribution welcome

--- src/index.cpp
+++ src/index.cpp
@@ -1,19 +1,62 @@
+//
+//  index.cpp
+//  Copyright (c) 2014-2018, MyMonero.com
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are
+//  permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//	conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//	of conditions and the following disclaimer in the documentation and/or other
+//	materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors may be
+//	used to endorse or promote products derived from this software without specific
+//	prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+//  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+//  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//
 #include <stdio.h>
 #include <emscripten/bind.h>
-
-using namespace emscripten;
-
-float foo(float a, float b, float t)
-{
-    return (1 - t) * a + t * b;
-}
-extern "C" {
-
-	#include "crypto-ops.h"
-}
+//
+#include "serial_bridge_index.hpp"
+//
 EMSCRIPTEN_BINDINGS(my_module)
 { // C++ -> JS
-    function("foo", &foo);
+	//
+    emscripten::function("create_transaction", &serial_bridge::create_transaction);
+    //
+    emscripten::function("decode_address", &serial_bridge::decode_address);
+    emscripten::function("is_subaddress", &serial_bridge::is_subaddress);
+    emscripten::function("is_integrated_address", &serial_bridge::is_integrated_address);
+    //
+    emscripten::function("new_integrated_address", &serial_bridge::new_integrated_address);
+    emscripten::function("new_fake_address_for_rct_tx", &serial_bridge::new_fake_address_for_rct_tx);
+    emscripten::function("new_payment_id", &serial_bridge::new_payment_id);
+    //
+    emscripten::function("newly_created_wallet", &serial_bridge::newly_created_wallet);
+    emscripten::function("mnemonic_from_seed", &serial_bridge::mnemonic_from_seed);
+    emscripten::function("seed_and_keys_from_mnemonic", &serial_bridge::seed_and_keys_from_mnemonic);
+    emscripten::function("validate_components_for_login", &serial_bridge::validate_components_for_login);
+    //
+    emscripten::function("estimate_rct_tx_size", &serial_bridge::estimate_rct_tx_size);
+    emscripten::function("calculate_fee", &serial_bridge::calculate_fee);
+    emscripten::function("estimated_tx_network_fee", &serial_bridge::estimated_tx_network_fee);
+    //
+    emscripten::function("generate_key_image", &serial_bridge::generate_key_image);
 }
 extern "C"
 { // C -> JS
