d60df7640241f833f9c1ec74befca063ba95e5da
Paul Shapiro <paulshapiro@users.noreply.github.com>
2018-10-21 14:00:14 -0500

    archive-emcpp: comments about possible cp cmd errors

--- bin/archive-emcpp.sh
+++ bin/archive-emcpp.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 bin/build-emcpp.sh &&
-cp build/MyMoneroCoreCpp.js monero_utils/;
+cp build/MyMoneroCoreCpp.js monero_utils/; 
+# it's ok if certain combinations of the following error.
+# e.g. under wasm builds, you wouldn't want cp .wasm to be erroring.
+# under asmjs builds with .mem file enabled, you'd not want .mem to be erroring.
 cp build/MyMoneroCoreCpp.wasm monero_utils/;
 cp build/MyMoneroCoreCpp.wast monero_utils/;
 cp build/MyMoneroCoreCpp.js.mem monero_utils/ 
