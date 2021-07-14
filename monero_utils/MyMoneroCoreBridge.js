1e6baa0c2e2e9d0b64c9e7ef8c54f9cf03f44700
dractw <dractw@gmail.com>
2021-07-14 17:52:47 +0300

    wasm binary as base64

--- monero_utils/MyMoneroCoreBridge.js
+++ monero_utils/MyMoneroCoreBridge.js
@@ -28,78 +28,68 @@
 //
 const MyMoneroCoreBridgeClass = require('./MyMoneroCoreBridgeClass')
 const MyMoneroBridge_utils = require('./MyMoneroBridge_utils')
-//
+const asmModule = require('./MyMoneroCoreCpp_ASMJS.asm')
+const MyMoneroCoreCpp_ASMJS = require("./MyMoneroCoreCpp_ASMJS")
+const path = require('path')
+const pathFormat = require('path-format')
+
+function locateFile(filename, scriptDirectory)
+{
+	let this_scriptDirectory = scriptDirectory
+	let fullPath = null; // add trailing slash to this
+	const lastChar = this_scriptDirectory.charAt(this_scriptDirectory.length - 1)
+
+	if (lastChar == "/" || lastChar == "\\") {
+		// ^-- this is not a '\\' on Windows because emscripten actually appends a '/'
+		this_scriptDirectory = this_scriptDirectory.substring(0, this_scriptDirectory.length - 1) // remove trailing "/"
+	}
+
+	fullPath = pathFormat({
+		dir: this_scriptDirectory,
+		base: filename,
+	})
+
+	if (fullPath == null) {
+		throw "Unable to derive fullPath. Please pass locateFile() to bridge obj init."
+	}
+
+	return fullPath
+}
+
 module.exports = function(options)
 {
 	options = options || {}
 
 	MyMoneroBridge_utils.update_options_for_fallback_to_asmjs(options)
 
-	const platform_info = MyMoneroBridge_utils.detect_platform();
-	const ENVIRONMENT_IS_WEB = platform_info.ENVIRONMENT_IS_WEB;
-	const ENVIRONMENT_IS_WORKER = platform_info.ENVIRONMENT_IS_WORKER;
-	const ENVIRONMENT_IS_NODE = platform_info.ENVIRONMENT_IS_NODE;
-	const ENVIRONMENT_IS_SHELL = platform_info.ENVIRONMENT_IS_SHELL;
-	//
-	function locateFile(filename, scriptDirectory)
-	{
-		// if (options["locateFile"]) {
-		// 	return options["locateFile"](filename, scriptDirectory)
-		// }
-		var this_scriptDirectory = scriptDirectory
-		const lastChar = this_scriptDirectory.charAt(this_scriptDirectory.length - 1)
-		if (lastChar == "/" || lastChar == "\\") { 
-			// ^-- this is not a '\\' on Windows because emscripten actually appends a '/'
-			this_scriptDirectory = this_scriptDirectory.substring(0, this_scriptDirectory.length - 1) // remove trailing "/"
-		}
-		var fullPath = null; // add trailing slash to this
-		if (ENVIRONMENT_IS_NODE) {
-			const path = require('path')
-			const lastPathComponent = path.basename(this_scriptDirectory)
-			if (lastPathComponent == "monero_utils") { // typical node or electron-main process
-				fullPath = path.format({
-					dir: this_scriptDirectory,
-					base: filename
-				})
-			} else {
-				console.warn(`MyMoneroCoreBridge/locateFile() on node.js didn't find "monero_utils" (or possibly MyMoneroCoreBridge.js) itself in the expected location in the following path. The function may need to be expanded but it might in normal situations be likely to be another bug. ${pathTo_cryptonoteUtilsDir}`)
-			}
-		} else if (ENVIRONMENT_IS_WEB) {
-			var pathTo_cryptonoteUtilsDir;
-			if (typeof __dirname !== undefined && __dirname !== "/") { // looks like node running in browser.. (but not going to assume it's electron-renderer since that should be taken care of by monero_utils.js itself)
-				// but just in case it is... here's an attempt to support it
-				// have to check != "/" b/c webpack (I think) replaces __dirname
-				pathTo_cryptonoteUtilsDir = "file://" + __dirname + "/" // prepending "file://" because it's going to try to stream it
-			} else { // actual web browser
-				pathTo_cryptonoteUtilsDir = this_scriptDirectory + `/mymonero_core_js/monero_utils/` // this works for the MyMonero browser build, and is quite general, at least
-			}
-			fullPath = pathTo_cryptonoteUtilsDir + filename
-		}
-		if (fullPath == null) {
-			throw "Unable to derive fullPath. Please pass locateFile() to bridge obj init."
-		}
-		//
-		return fullPath
-	}
 	return new Promise(function(resolve, reject) {
-		var Module_template = {}
+		const Module_template = {}
+
 		if (options.asmjs != true || options.wasm == true) { // wasm
 			console.log("Using wasm: ", true)
-			//
+
 			Module_template["locateFile"] = locateFile
-			//
+
 			// NOTE: This requires src/module-post.js to be included as post-js in CMakeLists.txt under a wasm build
-			require(`./MyMoneroCoreCpp_WASM`)(Module_template).ready.then(function(thisModule) 
+			require(`./MyMoneroCoreCpp_WASM`)(Module_template).ready.then(function(thisModule)
 			{
 				const instance = new MyMoneroCoreBridgeClass(thisModule);
+
 				resolve(instance);
 			}).catch(function(e) {
 				console.error("Error loading WASM_MyMoneroCoreCpp:", e);
 				reject(e);
-			});
-		} else { // this is synchronous so we can resolve immediately
+			})
+		} else {
 			console.log("Using wasm: ", false)
-			throw new Error('ASMJS version has been removed in this fork')
-    }
+
+			Module_template["asm"] = asmModule
+
+			setTimeout(function()
+			{ // "delaying even 1ms is enough to allow compilation memory to be reclaimed"
+				resolve(new MyMoneroCoreBridgeClass(MyMoneroCoreCpp_ASMJS(Module_template)))
+			}, 1)
+		}
+
 	});
 };
